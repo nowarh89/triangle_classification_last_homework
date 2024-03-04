ad03f6c789df0bc2ced2f8a1bd4b3a1eb895d4d6
Admin <Admin@DESKTOP-5NKFECA>
2024-03-04 16:57:09 +0200

    triangle_classification

--- src/test/java/Selleniun_start_projects/Selleniun_start_projects/AppTest.java
+++ src/test/java/Selleniun_start_projects/Selleniun_start_projects/AppTest.java
@@ -1,54 +1,175 @@
 package Selleniun_start_projects.Selleniun_start_projects;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.junit.Before;
-import java.time.Duration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import static org.junit.Assert.assertNotEquals;
-
-import java.lang.Thread;
 import org.openqa.selenium.support.ui.Select;
+
 public class AppTest {
-	 
 	
-	 static WebDriver  browser;
+	static WebDriver  browser;
 	 
 	 @Before
 	  public void setup() {
 		 
-			  System.setProperty("webdriver.gecko.driver","C:\\geckodriver-v0.34.0-win32\\geckodriver.exe");
+		 System.setProperty("webdriver.gecko.driver", "C:\\Users\\Admin\\Downloads\\geckodriver.exe");
 		      browser = new FirefoxDriver();
 		  	  browser.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 			
-		      browser.get("https://testpages.eviltester.com/styled/validation/input-validation.html");
+		      browser.get("https://testpages.eviltester.com/styled/apps/triangle/triangle001.html");
 		
 	  }
-		
 	@Test
-	public void Tests1() {
-    	
+	public void Test1() {
+   	
 		
-			WebElement textBox = browser.findElement(By.id("firstname"));
-			textBox.sendKeys("chaya");
-			textBox = browser.findElement(By.id("surname"));
-			textBox.sendKeys("Zilberstein");
-			textBox = browser.findElement(By.id("age"));
-			textBox.sendKeys("21");
-			Select drpCountry = new Select(browser.findElement(By.name("country")));
-			drpCountry.selectByVisibleText("Israel");
-			String currentPage=browser.getCurrentUrl();
-			browser.findElement(By.cssSelector("input[type='submit']")).click();
-			String newPage = browser.getCurrentUrl();
-			assertNotEquals(currentPage, newPage);
-    }
-	
-	
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("1");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("3");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Error: Not a Triangle", actualType);
+   }
+	@Test
+	public void Test2() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("1");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("1");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Error: Not a Triangle", actualType);
+   }
+
+	@Test
+	public void Test3() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("3");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("6");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("3");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Error: Not a Triangle", actualType);
+   }
+	@Test
+	public void Test4() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("4");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Error: Not a Triangle", actualType);
+   }
+	@Test
+	public void Test5() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("3");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("4");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Scalene", actualType);
+   }
+	@Test
+	public void Test6() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("1");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("3");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("3");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Isosceles", actualType);
+   }
+	@Test
+	public void Test7() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("5");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("5");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Isosceles", actualType);
+   }
+	@Test
+	public void Test8() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("2");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("1");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Isosceles", actualType);
+   }
+	@Test
+	public void Test9() {
+   	
+		
+			WebElement textBox = browser.findElement(By.id("side1"));
+			textBox.sendKeys("3");
+			textBox = browser.findElement(By.id("side2"));
+			textBox.sendKeys("3");
+			textBox = browser.findElement(By.id("side3"));
+			textBox.sendKeys("3");
+	        String initialType = browser.findElement(By.id("triangle-type")).getText();
+	        WebElement button = browser.findElement(By.id("identify-triangle-action"));
+	        button.click();
+	        String actualType = browser.findElement(By.id("triangle-type")).getText();
+	        assertEquals("Equilateral", actualType);
+   }
 }
